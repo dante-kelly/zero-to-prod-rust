6cc67a44e6fc86cc32e76cdd1c9c93587be7bf03
Dante Kelly <dante@dantekelly.com>
2023-08-21 23:57:31 -0700

    Update formatting

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,8 +1,10 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
@@ -10,7 +12,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
