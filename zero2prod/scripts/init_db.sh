e38857582a29c946421e55e6b08c7344785fc268
Dante Kelly <dante@dantekelly.com>
2023-08-23 22:36:19 -0700

    Modify init_db.sh script for github actions

--- zero2prod/scripts/init_db.sh
+++ zero2prod/scripts/init_db.sh
@@ -7,11 +7,11 @@ echo >&2 "Error: psql is not installed."
 exit 1
 fi
 
-if ! [ -x "$(command -v cargo sqlx)" ]; then
+if ! [ -x "$(command -v sqlx)" ]; then
 echo >&2 "Error: sqlx is not installed."
 echo >&2 "Use:"
 echo >&2 "
- cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+ cargo install --version=0.7.1 sqlx-cli --no-default-features --features rustls,postgres"
 echo >&2 "to install it."
 exit 1
 fi
